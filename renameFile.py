3a48fd7f2c51400fca48bbc2f93b5b89db27bc5c
nglthu <nglthu@gmail.com>
2016-11-16 15:04:50 +1300

    tkMessageBox

--- renameFile.py
+++ renameFile.py
@@ -1,8 +1,16 @@
 import os
+try:
+    # for Python2
+    from Tkinter import *   ## notice capitalized T in Tkinter 
+except ImportError:
+    # for Python3
+    from tkinter import *   ## notice lowercase 't' in tkinter here
+import tkinter.messagebox
 def rename_file():
     file_list = os.listdir(r"E:\BOOKS\CS\FullStackWebsite\rename\prank")
     save_path = os.getcwd()
     print(save_path)
+    tkinter.messagebox.showinfo("Say Hello", "Testing")
     os.chdir(r"E:\BOOKS\CS\FullStackWebsite\rename\prank")
     to_remove = "0123456789"
     table = {ord(char): None for char in to_remove}
