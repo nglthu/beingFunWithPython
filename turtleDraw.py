f62ca38b8efeab0f969bd89ac42f087fa9f43458
nglthu <nglthu@gmail.com>
2016-11-16 16:09:44 +1300

    customised turtle

--- turtleDraw.py
+++ turtleDraw.py
@@ -4,6 +4,9 @@ def draw_square():
     window = turtle.Screen()
     window.bgcolor("green")
     draw = turtle.Turtle()
+    draw.shape("turtle")
+    draw.color("yellow")
+    draw.speed(4)
     draw.forward(100)
     draw.right(90)
     draw.forward(100)
@@ -12,6 +15,8 @@ def draw_square():
     draw.right(90)
     draw.forward(100)
     draw.right(90)
+
+    window.exitonclick()
 
 
 draw_square()
