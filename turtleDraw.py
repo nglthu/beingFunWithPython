0eec392d43bafd441b82ce6275bec9943c7de622
nglthu <nglthu@gmail.com>
2016-11-16 23:09:40 +1300

    circle of squares

--- turtleDraw.py
+++ turtleDraw.py
@@ -12,15 +12,33 @@ def draw_art():
     window.bgcolor("green")
     square = turtle.Turtle()
     #draw square
-    draw(square,4,90,"yellow", "turtle")
+    for x in range(1,37):
+        square.right(10)
+        draw(square,4,90,"yellow", "turtle")
     #draw circle
-    circle = turtle.Turtle()
-    circle.shape("arrow")
-    circle.color("yellow")
-    circle.circle(100)
+##    circle = turtle.Turtle()
+##    circle.shape("arrow")
+##    circle.color("yellow")
+##    circle.circle(100)
+    #draw triangle
+##    triangle = turtle.Turtle()
+##    draw(triangle, 3, 120, "yellow", "turtle")
+    window.exitonclick() #to exit
+
+def draw_follower():
+    window = turtle.Screen()
+    window.bgcolor("green")
+    square = turtle.Turtle()
     #draw triangle
     triangle = turtle.Turtle()
-    draw(triangle, 3, 120, "yellow", "turtle")
+    for x in range(1,37):
+        triangle.right(20)
+        draw(triangle, 3, 120, "yellow", "turtle")
     window.exitonclick() #to exit
     
-draw_art()
+#draw_art()
+draw_follower()
+
+
+
+    
