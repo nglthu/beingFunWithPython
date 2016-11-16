58aedae61b16505495ba42015ea1c82712aa848a
nglthu <nglthu@gmail.com>
2016-11-16 16:03:12 +1300

    turtle

---
+++ turtleDraw.py
@@ -0,0 +1,17 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("green")
+    draw = turtle.Turtle()
+    draw.forward(100)
+    draw.right(90)
+    draw.forward(100)
+    draw.right(90)
+    draw.forward(100)
+    draw.right(90)
+    draw.forward(100)
+    draw.right(90)
+
+
+draw_square()
