62d1c6eacaedd48e03b7527bdffa60ff0f2b1ffd
nglthu <nglthu@gmail.com>
2018-07-12 15:11:44 +1200

    rename.py

--- renameFile.py
+++ renameFile.py
@@ -1,4 +1,7 @@
 import os
+import urllib.request
+import re
+
 try:
     # for Python2
     from Tkinter import *   ## notice capitalized T in Tkinter 
@@ -6,12 +9,17 @@ except ImportError:
     # for Python3
     from tkinter import *   ## notice lowercase 't' in tkinter here
 import tkinter.messagebox
+
+print(os.getcwd())
+currentPath = os.getcwd()
+filePath = currentPath+"/prank"
+print(filePath)
 def rename_file():
-    file_list = os.listdir(r"E:\BOOKS\CS\FullStackWebsite\rename\prank")
+    file_list = filePath
     save_path = os.getcwd()
     print(save_path)
     tkinter.messagebox.showinfo("Say Hello", "Testing")
-    os.chdir(r"E:\BOOKS\CS\FullStackWebsite\rename\prank")
+    os.chdir(filePath)
     to_remove = "0123456789"
     table = {ord(char): None for char in to_remove}
     for file_name in file_list:
